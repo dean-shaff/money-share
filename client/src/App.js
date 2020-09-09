d48d0009c7225e86002d07d60c73c07616ae8418
Dean Shaff <dean.shaff@gmail.com>
2020-09-09 14:45:52 -0500

    getting reach router to work as expected

--- client/src/App.js
+++ client/src/App.js
@@ -2,28 +2,39 @@ import React from "react";
 import { render } from "react-dom";
 import { Router, Link } from "@reach/router";
 
+
+import Home from "./components/Home.js"
+import Login from "./components/Login.js"
+import Register from "./components/Register.js"
+
 const App = ({ children }) => (
   <div>
-    <nav>
-      <Link to="/">Home</Link> <Link to="dashboard">Dashboard</Link>
-    </nav>
-    <Router>
-      <Home path="/" />
-      <Dashboard path="dashboard" />
-    </Router>
+  <nav className="navbar is-spaced has-shadow" role="navigation" aria-label="main navigation">
+    <div className="container">
+      <div className="navbar-brand">
+        <Link to="/" className="navbar-item">
+          <h1 className="title is-1">Money Share App</h1>
+        </Link>
+      </div>
+    </div>
+    <div className="navbar-menu">
+      <div className="navbar-end">
+        <div className="navbar-item">
+          <div className="buttons">
+            <Link to="register/" className="button is-primary">Sign Up</Link>
+            <Link to="login/" className="button is-light">Login</Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+  <Router>
+    <Home path="/" />
+    <Login path="login/" />
+    <Register path="register/" />
+  </Router>
   </div>
 );
 
-const Home = () => (
-  <div>
-    <h2>Welcome</h2>
-  </div>
-)
-
-const Dashboard = () => (
-  <div>
-    <h2>Dashboard</h2>
-  </div>
-)
 
 export default App
--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from "react-dom";
 
 import App from "./App.js";
 
-// import "./../node_modules/bulma/css/bulma.css";
+import "./../../node_modules/bulma/css/bulma.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
