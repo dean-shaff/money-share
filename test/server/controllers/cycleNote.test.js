aca593ab02ac18c25a860712675b6218c12618dc
Dean Shaff <dean.shaff@gmail.com>
2020-09-16 11:25:08 -0600

    commit tests

---
+++ test/server/controllers/cycleNote.test.js
@@ -0,0 +1,99 @@
+"use strict"
+
+const moment = require('moment')
+const { expect } = require('@hapi/code');
+const { init } = require("./../../../lib/server.js")
+
+const controllers = require("./../../../lib/controllers/")
+
+
+describe("cycleNote", () => {
+
+  const rotationId = '0'
+  let server
+  let newUser
+  let newCycleNote
+
+  beforeEach(async () => {
+    server = await init();
+    newUser = await controllers.user.create({
+      payload: {
+        name: "Dean Shaff",
+        username: "deanshaff",
+        email: "me@address.com",
+        password: "deanshaffpassword"
+      }
+    })
+
+    newCycleNote = await controllers.cycleNote.create({
+      params: {
+        userId: newUser.id
+      },
+      payload: {
+        'datePaid': moment(),
+        'amountPaid': 100.0,
+        'rotationId': rotationId
+      }
+    })
+  })
+
+  afterEach(async () => {
+    await server.stop();
+  });
+
+  test("POST /api/user/{userId}/cycleNote", async () => {
+    // console.log("POST /api/user/{userId}/cycleNote")
+    const res = await server.inject({
+      method: 'POST',
+      url: `/api/user/${newUser.id}/cycleNote`,
+      payload: {
+        'datePaid': moment(),
+        'amountPaid': 100.0,
+        'rotationId': rotationId
+      }
+    })
+    expect(res.statusCode).to.equal(200)
+  })
+
+  test("POST /api/user/{userId}/cycleNote without rotation Id", async () => {
+    const res = await server.inject({
+      method: 'POST',
+      url: `/api/user/${newUser.id}/cycleNote`,
+      payload: {
+        'datePaid': moment(),
+        'amountPaid': 100.0
+      }
+    })
+    expect(res.statusCode).to.equal(400)
+  })
+  test("GET /api/user/{userId}/cycleNote/{id}", async () => {
+    const res = await server.inject({
+      method: "GET",
+      url: `/api/user/${newUser.dataValues.id}/cycleNote/${newCycleNote.id}`
+    })
+    expect(res.statusCode).to.equal(200)
+    expect(res.result.amountPaid).to.equal(100.0)
+  })
+  //
+  test("PUT /user/{id}", async () => {
+    const res = await server.inject({
+      method: "PUT",
+      url: `/api/user/${newUser.dataValues.id}/cycleNote/${newCycleNote.id}`,
+      payload: {
+        amountPaid: 200.0
+      }
+    })
+    expect(res.statusCode).to.equal(200)
+    expect(res.result.amountPaid).to.equal(200.0)
+  })
+
+  test("DELETE /api/users/{userId}/cycleNote", async () => {
+
+    const res = await server.inject({
+      method: 'DELETE',
+      url: `/api/user/${newUser.id}/cycleNote/${newCycleNote.id}`
+    })
+    expect(res.statusCode).to.equal(204)
+  })
+
+})
---
+++ test/server/controllers/user.test.js
@@ -0,0 +1,101 @@
+"use strict"
+
+const { expect } = require('@hapi/code');
+const { init } = require("./../../../lib/server.js")
+
+const user = require("./../../../lib/controllers/user.js")
+
+
+describe("user", () => {
+
+  let server
+  let newUser
+
+  beforeEach(async () => {
+    server = await init();
+    newUser = await user.create({
+      payload: {
+        name: "Dean Shaff",
+        username: "deanshaff",
+        email: "me@address.com",
+        password: "deanshaffpassword"
+      }
+    })
+  })
+
+  afterEach(async () => {
+    await server.stop();
+  });
+
+  test("POST /api/user", async () => {
+    const res = await server.inject({
+      method: "POST",
+      url: "/api/user",
+      payload: {
+        username: "firstlast",
+        name: "first last",
+        email: "first.last@address.com",
+        password: "firstlastpassword"
+      }
+    })
+    expect(res.statusCode).to.equal(200)
+  })
+
+  test("POST /api/user with same email", async () => {
+    const res = await server.inject({
+      method: "POST",
+      url: "/api/user",
+      payload: {
+        username: "firstlast",
+        name: "first last",
+        email: "me@address.com",
+        password: "firstlastpassword"
+      }
+    })
+    expect(res.statusCode).to.equal(400);
+  })
+
+  test("POST /api/user with same username", async () => {
+    const res = await server.inject({
+      method: "POST",
+      url: "/api/user",
+      payload: {
+        username: "deanshaff",
+        name: "first last",
+        email: "first.last@address.com",
+        password: "firstlastpassword"
+      }
+    })
+    expect(res.statusCode).to.equal(400);
+  })
+
+  test("GET /api/user/{id}", async () => {
+    const res = await server.inject({
+      method: "GET",
+      url: `/api/user/${newUser.dataValues.id}`
+    })
+    expect(res.statusCode).to.equal(200)
+    expect(res.result.name).to.equal("Dean Shaff")
+  })
+
+  test("PUT /api/user/{id}", async () => {
+    const res = await server.inject({
+      method: "PUT",
+      url: `/api/user/${newUser.dataValues.id}`,
+      payload: {
+        name: "sruti"
+      }
+    })
+    expect(res.statusCode).to.equal(200)
+    expect(res.result.name).to.equal("sruti")
+  })
+
+  test("DELETE /api/user/{id}", async () => {
+    const res = await server.inject({
+      method: "DELETE",
+      url: `/api/user/${newUser.dataValues.id}`
+    })
+    expect(res.statusCode).to.equal(204);
+  })
+
+})
